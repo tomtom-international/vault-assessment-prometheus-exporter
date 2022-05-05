e20f7fa86d9f38c5fdd0d218967a78250e3c2432
Eugene Davis <eugene.davis@tomtom.com>
2022-05-05 12:54:31 +0200

    [DPT-3789] Initial implementation (#2)
    * Add basic expiration monitor, script for setting metadata for a secret, and refactor a bit
    
    * Fix metadata setting functionality, fully add support for custom metadata
    
    * Separate out secret monitor creation
    
    * Add service name into metric name
    
    * Add recursion
    
    * Swap to a  single metric name differentiated by labels, make labels into a class level property
    
    * Add inital vault authentication code
    
    * Set new default port
    
    * Some pylint cleanup, and reorg into proper package structure
    
    * Updates
    
    * Improve scripts and add pylint fixes
    
    * Pylint cleanup for common
    
    * Improve readability for create_monitors.py
    
    * Complete pylint formatting
    
    * Update package name for pylint in workflow and move pylint above mypy
    
    * Add mypy fixes
    
    * Apply formatting
    
    * Update test skeleton with new package structure
    
    * Fix args typo
    
    * Fixed typo

---
+++ vault_monitor/common/vault_authenticate.py
@@ -0,0 +1,136 @@
+"""
+Functions for getting Hashicorp Vault client
+"""
+import os
+import logging
+import warnings
+
+from typing import Dict
+
+import hvac
+
+LOGGER = logging.getLogger("vault_authenticate")
+
+
+def get_vault_client_for_user(url: str = None, namespace: str = None, vault_token: str = None) -> hvac.Client:
+    """
+    Gets a HVAC Vault client instance configured against Vault, targeted towards end-user systems (checks for environmental variables and existing token in .vault-token)
+    """
+    url = get_address(url)
+    namespace = get_namespace(namespace)
+    if not vault_token:
+        LOGGER.debug("Attempting to read Vault token from file.")
+        with open(os.path.expanduser("~/.vault-token"), "r", encoding="utf8") as token_file:
+            vault_token = token_file.read()
+
+    # If the values still aren't set, raise an error or warn user as appropriate
+    if not url:
+        raise RuntimeError("Unable to get Vault URL.\nPlease provide it as an argument or set the VAULT_ADDR environmental variable.")
+    if not namespace:
+        LOGGER.info("No namespace provided, so using root namespace.")
+    if not vault_token:
+        raise RuntimeError("Unable to get Vault token.\nPlease execute `vault login`, provide it as an argument or set the VAULT_ADDR environmental variable.")
+
+    return hvac.Client(url=url, token=vault_token, namespace=namespace)
+
+
+def get_authenticated_client(auth_config: Dict[str, Dict[str, str]], address: str, namespace: str) -> hvac.Client:
+    """
+    Returns an authenticated Vault client as configured by the authentication section of the configuration file.
+    """
+    namespace = get_namespace(namespace)
+    address = get_address(address)
+
+    if len(auth_config) > 1:
+        warnings.warn("Multiple authentication methods are selected. Only the highest priority one will be used, please review your configuration!")
+
+    approle_auth_config = auth_config.get("approle", None)
+    kubernetes_auth_config = auth_config.get("kubernetes", None)
+    token_auth_config = auth_config.get("token", {})
+
+    if approle_auth_config:
+        return get_client_with_approle_auth(approle_auth_config, address, namespace)
+
+    if kubernetes_auth_config:
+        return get_client_with_kubernetes_auth(kubernetes_auth_config, address, namespace)
+
+    # As a last ditch effort check for tokens, this includes checking for sensible defaults in case of limited configuration
+    return get_client_with_token_auth(token_auth_config, address, namespace)
+
+
+def get_namespace(namespace: str = None) -> str:
+    """
+    In the event that namespace is None, return the value for VAULT_NAMESPACE if that is set
+    """
+    # Checks explicitly for None, an empty string is a valid option
+    if namespace is None:
+        namespace = str(os.getenv("VAULT_NAMESPACE", ""))
+    return namespace
+
+
+def get_address(address: str = None) -> str:
+    """
+    If the Vault address isn't set, check the contents of the VAULT_ADDR environmental variable and return it.
+    """
+    if not address:
+        address = str(os.getenv("VAULT_ADDR", ""))
+    return address
+
+
+def get_client_with_approle_auth(config: Dict[str, str], address: str, namespace: str) -> hvac.Client:
+    """
+    Returns an authenticated Vault client with approle authentication.
+    """
+    role_id = config.get("role_id", None)
+    if not role_id:
+        role_id_variable = config.get("role_id_variable", None)
+        if role_id_variable:
+            role_id = os.getenv(role_id_variable)
+        else:
+            role_id_filename = config.get("role_id_file", None)
+            if role_id_filename:
+                with open(role_id_filename, "r", encoding="UTF8") as role_id_file:
+                    role_id = role_id_file.read()
+
+    secret_id = config.get("secret_id", None)
+    if not secret_id:
+        secret_id_variable = config.get("secret_id_variable", None)
+        if secret_id_variable:
+            secret_id = os.getenv(secret_id_variable)
+        else:
+            secret_id_filename = config.get("secret_id_file", None)
+            if secret_id_filename:
+                with open(secret_id_filename, "r", encoding="UTF8") as secret_id_file:
+                    secret_id = secret_id_file.read()
+
+    client = hvac.Client(url=address, namespace=namespace)
+    return client.auth.approle.login(role_id=role_id, secret_id=secret_id)
+
+
+def get_client_with_kubernetes_auth(config: Dict[str, str], address: str, namespace: str) -> hvac.Client:
+    """
+    Returns an authenticated Vault client with Kuberenetes authentication.
+    """
+    mount_point = config.get("mount_point", "kubernetes")
+    jwt_file_path = config.get("token_file", "/var/run/secrets/kubernetes.io/serviceaccount/token")
+    with open(jwt_file_path, "r", encoding="UTF8") as jwt_file:
+        jwt = jwt_file.read()
+    client = hvac.Client(url=address, namespace=namespace)
+    return client.auth_kubernetes(mount_point, jwt)
+
+
+def get_client_with_token_auth(config: Dict[str, str], address: str, namespace: str) -> hvac.Client:
+    """
+    Returns an authenticated Vault client using token authentication.
+    """
+    token_var_name = config.get("token_var_name", None)
+    token_filename = config.get("token_file", None)
+    vault_token = None
+    if token_var_name:
+        vault_token = os.getenv(token_var_name, None)
+    elif token_filename:
+        with open(os.path.expanduser(token_filename), "r", encoding="utf8") as token_file:
+            vault_token = token_file.read()
+
+    # If vault_token is none, the hvac client will check for sensible defaults during init (VAULT_TOKEN and ~/.vault-token)
+    return hvac.Client(url=address, token=vault_token, namespace=namespace)
---
+++ vault_monitor/scripts/set_expiration.py
@@ -0,0 +1,148 @@
+"""
+Updates the last-updated and expiration date-time fields for a given secret
+"""
+
+import logging
+import argparse
+import warnings
+
+import requests
+
+from vault_monitor.common.vault_authenticate import get_vault_client_for_user
+from vault_monitor.expiration_monitor.vault_time import ExpirationMetadata
+
+LOGGER = logging.getLogger("set_expiration")
+
+# Disable certain things for scripts only, as over-doing the DRY-ness of them can cause them to be less useful as samples
+# pylint: disable=duplicate-code,too-many-arguments,too-many-locals
+
+
+def handle_args() -> argparse.Namespace:
+    """
+    Handles arg parser, returning the values it sets.
+    """
+    parser = argparse.ArgumentParser(description="Update a kv2 secret with expiration metadata.")
+
+    parser.add_argument("mount_point", type=str, help="Mount point of kv2 engine, e.g. secret")
+    parser.add_argument("secret_path", type=str, help="Path to secret, e.g. some/secret")
+
+    parser.add_argument(
+        "-l",
+        "--logging",
+        type=str,
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+        help="Set the log level.",
+    )
+
+    vault_group = parser.add_argument_group(title="Vault configuration")
+    vault_group.add_argument("--address", type=str, default=None, help="Sets the Vault address (defaults to checking $VAULT_ADDR).")
+    vault_group.add_argument("--namespace", type=str, default=None, help="Sets the Vault namespace (defaults to checking $VAULT_NAMESPACE).")
+
+    expiration_group = parser.add_argument_group(title="Expiration time", description="Configure the amount of time until expiration.")
+    expiration_group.add_argument("--weeks", default=0, type=int, help="Set the number of weeks before expiration.")
+    expiration_group.add_argument("--days", default=0, type=int, help="Set the number of days before expiration.")
+    expiration_group.add_argument("--hours", default=0, type=int, help="Set the number of hours before expiration.")
+    expiration_group.add_argument("--minutes", default=0, type=int, help="Set the number of minutes before expiration.")
+    expiration_group.add_argument("--seconds", default=0, type=int, help="Set the number of seconds before expiration.")
+
+    fieldnames_group = parser.add_argument_group(
+        title="Fieldnames", description="Configure the fieldnames to use (optional).\nThese must be matched by the exporter configuration for expiration monitoring."
+    )
+    fieldnames_group.add_argument("--last_renewed_timestamp_fieldname", default="last_renewal_timestamp", type=str, help="Set the fieldname to use for the last renewed timestamp.")
+    fieldnames_group.add_argument("--expiration_timestamp_fieldname", default="expiration_timestamp", type=str, help="Set the fieldname to use for the expiration timestamp.")
+
+    return parser.parse_args()
+
+
+def set_expiration(
+    address: str,
+    namespace: str,
+    mount_point: str,
+    secret_path: str,
+    weeks: int,
+    days: int,
+    hours: int,
+    minutes: int,
+    seconds: int,
+    last_renewed_timestamp_fieldname: str = "last_renewal_timestamp",
+    expiration_timestamp_fieldname: str = "expiration_timestamp",
+    log_level: str = "INFO",
+) -> None:
+    """
+    Sets expiration metadadate for specified secret.
+    """
+    # Get the hvac client, we will have to use requests some with the token it manages
+    vault_client = get_vault_client_for_user(url=address, namespace=namespace)
+
+    expiration_info = ExpirationMetadata.from_duration(weeks, days, hours, minutes, seconds, last_renewed_timestamp_fieldname, expiration_timestamp_fieldname)
+
+    logging.basicConfig(level=log_level)
+
+    LOGGER.info("Updating expiration data for secret.")
+
+    # Custom metadata isn't fully supported by hvac at the moment, use requests
+    response = requests.patch(
+        f"{vault_client.url}/v1/{mount_point}/metadata/{secret_path}",
+        headers={"X-Vault-Namespace": vault_client.adapter.namespace, "X-Vault-Token": vault_client.token, "Content-Type": "application/merge-patch+json"},
+        json={"custom_metadata": expiration_info.get_serialized_expiration_metadata()},
+    )
+
+    if response.status_code == 405:
+        warnings.warn(
+            "Received 405 error when attempting to PATCH metadata, using GET+PUT instead. This indicates an older version of Vault is in use (<10), support will eventually be dropped from this tool.",
+            DeprecationWarning,
+        )
+        response = requests.get(
+            f"{vault_client.url}/v1/{mount_point}/metadata/{secret_path}",
+            headers={"X-Vault-Namespace": vault_client.adapter.namespace, "X-Vault-Token": vault_client.token, "Content-Type": "application/merge-patch+json"},
+        )
+        response.raise_for_status()
+
+        # Take the existing metadata, clean up what we don't control, update it and then push it
+        metadata = response.json()
+
+        # When no custom_metadata is set, Vault will return None, so we have to set up an empty dictionary
+        if not metadata["data"]["custom_metadata"]:
+            metadata["data"]["custom_metadata"] = {}
+
+        metadata["data"]["custom_metadata"].update(expiration_info.get_serialized_expiration_metadata())
+        metadata = metadata["data"]
+        del metadata["created_time"]
+        del metadata["current_version"]
+        del metadata["oldest_version"]
+        del metadata["updated_time"]
+        del metadata["versions"]
+
+        response = requests.put(
+            f"{vault_client.url}/v1/{mount_point}/metadata/{secret_path}",
+            headers={"X-Vault-Namespace": vault_client.adapter.namespace, "X-Vault-Token": vault_client.token},
+            json=metadata,
+        )
+
+    response.raise_for_status()
+
+
+def main() -> None:
+    """
+    Gets the arguments and passes them to set_expiration function.
+    """
+    args = handle_args()
+    set_expiration(
+        args.address,
+        args.namespace,
+        args.mount_point,
+        args.secret_path,
+        args.weeks,
+        args.days,
+        args.hours,
+        args.minutes,
+        args.seconds,
+        args.last_renewed_timestamp_fieldname,
+        args.expiration_timestamp_fieldname,
+        args.logging,
+    )
+
+
+if __name__ == "__main__":
+    main()
