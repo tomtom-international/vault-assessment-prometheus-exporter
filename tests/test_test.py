85dff27daaf721f1d08c4219d6f0002896cc9a3d
Eugene Davis <eugene.davis@tomtom.com>
2022-06-10 09:42:52 +0200

    Improve Handling of Time Data (#25)
    * Prepped for adding tests
    
    * Handle malformed timestamps, improve handling for missing timestamps
    
    * Add tests for vault_time module
    
    * Update poetry.lock with correct dependency setup
    
    * Bump version: 0.3.8 → 0.3.9
    
    * Correct UTC handling by swapping to timezone aware usage of datetime
    
    * Apply formatting
    
    * Update pytest execution to properly include coverage

--- tests/test_test.py
+++
@@ -1,8 +0,0 @@
-import vault_monitor.secret_expiration_monitor
-
-
-def test_test():
-    """
-    Example test
-    """
-    pass
--- vault_monitor/secret_expiration_monitor/vault_time.py
+++ vault_monitor/secret_expiration_monitor/vault_time.py
@@ -1,9 +1,9 @@
 """
 Wraps time handling calls to ensure consistent formatting
 """
-
+import logging
 from typing import Dict, TypeVar, Type
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 ExpirationMetadataType = TypeVar("ExpirationMetadataType", bound="ExpirationMetadata")  # pylint: disable=invalid-name
 
@@ -28,13 +28,13 @@ class ExpirationMetadata:
         expiration_hours: int,
         expiration_minutes: int,
         expiration_seconds: int,
-        last_renewed_timestamp_fieldname: str,
-        expiration_timestamp_fieldname: str,
+        last_renewed_timestamp_fieldname: str = "last_renewed_timestamp",
+        expiration_timestamp_fieldname: str = "expiration_timestamp",
     ) -> ExpirationMetadataType:
         """
         Creates an instance of ExpirationMetadata from the current time for last_renewed_time and gets the expiration from duration input
         """
-        last_renewed_time = datetime.utcnow()
+        last_renewed_time = datetime.now(timezone.utc)
         expiration_delta = timedelta(weeks=expiration_weeks, days=expiration_days, hours=expiration_hours, minutes=expiration_minutes, seconds=expiration_seconds)
 
         expiration_time = last_renewed_time + expiration_delta
@@ -43,23 +43,33 @@ class ExpirationMetadata:
 
     # Used when reading from a secret
     @classmethod
-    def from_metadata(cls: Type[ExpirationMetadataType], metadata: dict, last_renewed_timestamp_fieldname: str, expiration_timestamp_fieldname: str) -> ExpirationMetadataType:
+    def from_metadata(
+        cls: Type[ExpirationMetadataType], metadata: dict, last_renewed_timestamp_fieldname: str = "last_renewed_timestamp", expiration_timestamp_fieldname: str = "expiration_timestamp"
+    ) -> ExpirationMetadataType:
         """
         Creates an instance of ExpirationMetadata based on custom_metadata from the secret.
         """
         last_renewed_timestamp = metadata.get(last_renewed_timestamp_fieldname, None)
         expiration_timestamp = metadata.get(expiration_timestamp_fieldname, None)
 
-        # Missing fields means we go back to the 70s (for both dates - so well expired)
-        if last_renewed_timestamp:
+        # Missing fields or malformed timestamps means we go back to the 70s, should be very obvious to the user
+        try:
             last_renewed_time = cls.__get_time_from_iso_utc(last_renewed_timestamp)
-        else:
-            last_renewed_time = datetime.fromtimestamp(0)
+        except TypeError:
+            logging.error("Failed to get last_renewed_timestamp due to issues retrieving metadata for %s, setting to 1970.", last_renewed_timestamp_fieldname)
+            last_renewed_time = datetime.fromtimestamp(0, tz=timezone.utc)
+        except ValueError:
+            logging.error("Failed to parse last_renewed_timestamp for %s, setting to 1970.", last_renewed_timestamp_fieldname)
+            last_renewed_time = datetime.fromtimestamp(0, tz=timezone.utc)
 
-        if expiration_timestamp:
+        try:
             expiration_time = cls.__get_time_from_iso_utc(expiration_timestamp)
-        else:
-            expiration_time = datetime.fromtimestamp(0)
+        except TypeError:
+            logging.error("Failed to get expiration_timestamp due to issues retrieving metadata for %s, setting to 1970.", expiration_timestamp_fieldname)
+            expiration_time = datetime.fromtimestamp(0, tz=timezone.utc)
+        except ValueError:
+            logging.error("Failed to parse expiration_timestamp_field for %s, setting to 1970.", expiration_timestamp_fieldname)
+            expiration_time = datetime.fromtimestamp(0, tz=timezone.utc)
 
         return cls(last_renewed_time, expiration_time, last_renewed_timestamp_fieldname, expiration_timestamp_fieldname)
 
